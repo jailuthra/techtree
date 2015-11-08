551a572e0d0dff46807620d55acc34d866116770
Jai Luthra <me@jailuthra.in>
2015-11-08 05:33:01 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+TechTree
+=========
+
+Run the script to generate a dependency graph for courses of IIIT-D in DOT format
+
+Use the dot file to visualize the graph using any graphviz tool
---
+++ make_graph.py
@@ -0,0 +1,75 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import networkx as nx
+# import matplotlib.pyplot as plt
+
+class Course():
+    prereq = []
+    def __init__(self, id, title):
+        self.id = id
+        self.title = title
+    def __repr__(self):
+        return 'Course()'
+    def __str__(self):
+        return self.id + ' ' + self.title + ' Requires:' + ' '.join(self.prereq)
+
+with urllib.request.urlopen('http://sites.iiitd.ac.in/courserepo/') as res:
+    html = res.read() 
+
+soup = BeautifulSoup(html, 'html.parser')
+
+exTitle = lambda title: title.string.split('Title: ')[1][:-1]
+exId = lambda id: id.string.split('(')[0][:-1]
+exPRId = lambda pr_id: pr_id.string.strip(' ')
+
+# print(soup.prettify())
+titles = soup.find_all(attrs={'class': 'title'})
+ids = soup.find_all(attrs={'class': 'number'})
+
+prereqs = {}
+
+for id in ids:
+    pr_row = id.parent.parent.next_sibling.next_sibling.next_sibling.next_sibling
+    if 'Pre-requisite:' in str(pr_row):
+        prereqs[exId(id)] = [exPRId(pr_id) for pr_id in pr_row.find_all('a')]
+
+# print(prereqs)
+
+
+# titles = [exTitle(title) for title in titles]
+# ids = [exId(id) for id in ids]
+
+titles = map(exTitle, titles)
+ids = map(exId, ids)
+
+courses = []
+
+for t, i in zip(titles, ids):
+    course = Course(i, t)
+    if i in prereqs.keys():
+        course.prereq = prereqs[i]
+    courses.append(course)
+
+# for course in courses:
+    # print(course)
+
+# GRAPH
+
+#nodes = [course.id for course in courses]
+edges = []
+
+for course in courses:
+    if course.prereq != []:
+        for pr in course.prereq:
+            edges.append((pr, course.id))
+
+nodes = set([n1 for n1,n2 in edges] + [n2 for n1,n2 in edges])
+G = nx.DiGraph()
+for node in nodes:
+    G.add_node(node)
+for edge in edges:
+    G.add_edge(edge[0], edge[1])
+nx.write_dot(G, "graph.dot")
+# pos = nx.spring_layout(G)
+# nx.draw(G, pos)
+# plt.show()
